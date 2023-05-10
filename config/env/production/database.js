40060f7a1c0151fce28f07dc6764b9d1fd72ac25
Martin-Msb <martin.msba@gmail.com>
2023-05-11 00:06:03 +0200

    db settings

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,3 +1,8 @@
+const parse = require("pg-connection-string").parse;
+
+const { host, port, database, user, password } = parse(
+  process.env.DATABASE_URL
+);
 
 module.exports = ({ env }) => ({
     connection: {
@@ -9,7 +14,7 @@ module.exports = ({ env }) => ({
         user: env('DATABASE_USERNAME'),
         password: env('DATABASE_PASSWORD'),
         ssl: {
-            ca: env('CA_CERT'),
+        rejectUnauthorized:env.bool('DATABASE_SSL_SELF', false),
         },
     },
     debug: false,
