3fd1a4ae9a50f519780aae5ed72fa1eafffa53ce
Martin-Msb <martin.msba@gmail.com>
2023-09-14 22:37:57 +0200

    diagnosis

---
+++ src/api/diagnosis/controllers/diagnosis.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * diagnosis controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::diagnosis.diagnosis');
---
+++ src/api/diagnosis/routes/diagnosis.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * diagnosis router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::diagnosis.diagnosis');
